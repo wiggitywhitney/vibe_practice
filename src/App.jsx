3480e0a754861c497f6b0aba34a63bc5192064ec
Whitney Lee <wiggitywhitney@gmail.com>
2025-05-12 05:43:15 -0500

    Integrate AddSpiderButton and prepare for rainbow component refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { AddSpiderButton } from './components/AddSpiderButton'
 
 function App() {
   return (
     <div className="app-container">
       {/* Empty container for background only */}
+      <AddSpiderButton />
     </div>
   )
 }
